96d941367bc4cf55a372cd1d0c92a2f0d5944302
lieza dessein <dez@smartbe.be>
2017-06-05 15:44:18 +0200

    Initial commit with devise template from https://github.com/lewagon/rails-templates

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_CHANGE_THIS_TO_YOUR_RAILS_APP_NAME_session'
+Rails.application.config.session_store :cookie_store, key: '_yugen_session'
